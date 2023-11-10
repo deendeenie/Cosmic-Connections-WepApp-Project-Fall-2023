4fa0159f7126e09c89fa138a3e35146858e9180c
Deen Rotaru <danutadumitritarotaru@gmail.com>
2023-11-10 20:13:49 +0100

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Cosmic-Connections-WepApp-Project-Fall-2023/"}>
+            <App />
+        </BrowserRouter>
   </React.StrictMode>,
 )
