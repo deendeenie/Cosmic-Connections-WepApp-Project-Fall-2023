d7997d05c6e2ff67f4b4b7ad769d9f821eceb852
Deen Rotaru <danutadumitritarotaru@gmail.com>
2023-11-21 10:16:14 +0100

    Change of URL

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/cosmic-connections-wepApp-project-fall-2023/"}>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Cosmic-Connections-WepApp-Project-Fall-2023/"}>
             <App/>
         </BrowserRouter>
   </React.StrictMode>,
