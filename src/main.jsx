94cd91036202d739ac379e51df40b1c9ff704070
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-11-21 10:11:26 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from './CosmicConnectionsApp.jsx'
-import './index.css'
+import App from "./CosmicConnectionsApp.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/cosmic-connections-wepApp-project-fall-2023/"}>
-            <App/>
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter basename={import.meta.env.DEV ? "/" : "/Cosmic-Connections-WepApp-Project-Fall-2023/"}>
+            <App />
         </BrowserRouter>
-  </React.StrictMode>,
-)
+    </React.StrictMode>
+);
